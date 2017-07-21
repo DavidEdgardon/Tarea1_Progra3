92b7a33c192093270af3f3b7465a0bd34805fba6
DavidNu <davidedgardonu121@gmail.com>
2017-07-21 14:52:15 -0600

    Inicio

---
+++ cmake-build-debug/CMakeFiles/Tarea.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Tarea.dir/main.cpp.obj"
+  "CMakeFiles/Tarea.dir/Evaluador.cpp.obj"
+  "Tarea.pdb"
+  "Tarea.exe"
+  "Tarea.exe.manifest"
+  "libTarea.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Tarea.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
